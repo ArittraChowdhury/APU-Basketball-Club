92146ace21cbc1554c7fe8d6ebad25a1df6b675b
rheathadhani <arittrachy@gmail.com>
2023-11-06 10:28:14 +0800

    Initial Commit

---
+++ StrategyTraining.js
@@ -0,0 +1,19 @@
+﻿<script>
+// Get the video
+var vid = document.getElementById("myVideo");
+vid.volume = 0.5;
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
